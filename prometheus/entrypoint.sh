04a2950d055c78fd549bc64c397dfa83f78f8ac0
Arnaud Lafon <arnaud.lafon@gmail.com>
2025-03-24 17:26:08 +0100

    Add a json_exporter job to scrape block counts

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -3,5 +3,6 @@
 sed "s/{{TARAXA_NODE_VALIDATORS}}/${TARAXA_NODE_VALIDATORS}/g" /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.taraxa.yml
 sed -i "s/{{TARAXA_NODE_PROXY_AUTH_TYPE}}/${TARAXA_NODE_PROXY_AUTH_TYPE}/g" /etc/prometheus/prometheus.taraxa.yml
 sed -i "s/{{TARAXA_NODE_PROXY_AUTH_CREDENTIALS}}/${TARAXA_NODE_PROXY_AUTH_CREDENTIALS}/g" /etc/prometheus/prometheus.taraxa.yml
+sed -i "s/{{TARAXA_NODE_VALIDATORS_STATS_URL}}/${TARAXA_NODE_VALIDATORS_STATS_URL}/g" /etc/prometheus/prometheus.taraxa.yml
 
 exec /bin/prometheus "$@"
